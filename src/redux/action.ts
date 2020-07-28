11cfeabde2934ce36d9ed682c42558d5e5c04718
Mevan Samaratunga <msamaratunga@Mevans-MacBook-Pro.local>
2020-07-28 23:30:42 +0400

    function to combine props of a given list of objects

--- src/redux/action.ts
+++ src/redux/action.ts
@@ -226,3 +226,15 @@ export function combineEpicsWithGlobalErrorHandler(
       })
   );
 }
+
+/**
+ * Merges all properties in the given array of objects
+ * and returns an object with the properties combined.
+ * 
+ * @param propLists  List of objects whose properties should be combined
+ */
+export function combineProps(propLists: Object[]): Object {
+  let combinedList = {};
+  propLists.forEach(propList => Object.assign(combinedList, propList));
+  return combinedList;
+}

32524e4a2105c8669fb802b8ddf916fd0a15c6bd
Mevan Samaratunga <mevansam@gmail.com>
2021-05-11 09:11:32 -0400

    function to format a number into computer quantity units

---
+++ src/utility/format.ts
@@ -0,0 +1,14 @@
+/**
+ * Formats a value given in bytes 
+ * to human readable short form.
+ * 
+ * @param bytes number to format
+ * @returns number formated in short form
+ */
+export function bytesToSize(bytes: number): string {
+  const sizes: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes === 0) return 'n/a'
+  const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString())
+  if (i === 0) return `${bytes} ${sizes[i]}`
+  return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`
+}

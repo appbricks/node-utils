f78680c1f909e747b78d996063e71521ab7d0007
Mevan Samaratunga <mevansam@gmail.com>
2021-05-14 11:30:29 -0400

    format byte quantities

--- src/utility/format.ts
+++ src/utility/format.ts
@@ -6,7 +6,7 @@
  * @returns number formated in short form
  */
 export function bytesToSize(bytes: number): string {
-  const sizes: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  const sizes: string[] = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB']
   if (bytes === 0) return 'n/a'
   const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString())
   if (i === 0) return `${bytes} ${sizes[i]}`

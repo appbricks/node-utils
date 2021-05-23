bd061ab402781940dff5d8853851fc7ab189b90e
Mevan Samaratunga <mevansam@gmail.com>
2021-05-22 20:18:21 -0400

    fixed formating of byte quantities

--- src/utility/format.ts
+++ src/utility/format.ts
@@ -6,9 +6,19 @@
  * @returns number formated in short form
  */
 export function bytesToSize(bytes: number): string {
-  const sizes: string[] = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB']
-  if (bytes === 0) return 'n/a'
-  const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString())
-  if (i === 0) return `${bytes} ${sizes[i]}`
-  return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`
+  switch (bytes) {
+    case 0:
+      return '0 bytes';
+    case 1:
+      return '1 byte';      
+  }
+
+  const sizes: string[] = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB'];
+  const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
+
+  if (i === 0) {
+    return `${bytes} ${sizes[i]}`;
+  } else {
+    return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`;
+  }
 }
